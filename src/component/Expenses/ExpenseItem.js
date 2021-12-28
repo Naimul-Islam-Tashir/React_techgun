7bedd270caaf87222517175dad5ff9161d701d64
tashir1998 <naimul15-1885@diu.edu.bd>
2021-12-28 22:43:31 +0600

    State In React

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css'
-import Card from '../UI/Card'
-
+import './ExpenseItem.css';
+import Card from '../UI/Card';
 let ExpenseItem=(props)=>{
-    
+
+    const[title,setTitle]=useState(props.title); 
+    function changeFunction()
+    {
+      setTitle('New Title');
+    }
     return(
-        
         <Card className='expenseclass'>
            <ExpenseDate eDate={props.date} />
         <div>
-        <h2>TitleName : {props.title}</h2>
+        <h2>TitleName : {title}</h2>
         <h3>Price : {props.amount}</h3>
         </div>
+         <button onClick={ changeFunction }>ChangeButton</button>
         </Card>
     )
 }
